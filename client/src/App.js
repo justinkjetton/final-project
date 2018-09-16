b0eb83d2e8830a9c47cc661acf0e505b0e24d3bf
Chuma Uwazurike <35809975+chuma615@users.noreply.github.com>
2018-09-15 23:01:32 -0400

    Revert "added weather app file"

--- client/src/App.js
+++ client/src/App.js
@@ -1,103 +1,21 @@
-import React from "react";
-import Titles from "./Components/Titles";
-import Form from "./Components/Form";
-import Weather from "./Components/Weather";
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
 
-const API_KEY =  "751bbacb9e22b1852d7baaef0427451a"; 
-
-class App extends React.Component {
- state = {
-tempreture: undefined,
-city: undefined,
-country: undefined,
-humidity: undefined,
-description: undefined,
-error: undefined
-}
-
-showWeather = async (e) => {
-  e.preventDefault();
-
-  const city = e.target.elements.city.value;
-  const country = e.target.elements.country.value;
-
-  const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=${API_KEY}&units=Imperial`);
-
-  const data = await api_call.json();
-  console.log(data);
-  
-  if (city && country){
-   
-    this.setState ({
-  
-      tempreture: data.list[0].main.temp,
-      city: data.city.name,
-      country: data.city.country,
-      humidity: data.list[0].main.humidity,
-      description: data.list[0].weather[0].description,
-      error: ""
-  
-    });
-  } else if (city||country) {
-
-    this.setState({
-      tempreture: undefined,
-      city: undefined,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      error: "Error: Please input both City and Country!!"
-    
-    });
-      } 
-      else {
-
-this.setState({
-  tempreture: undefined,
-  city: undefined,
-  country: undefined,
-  humidity: undefined,
-  description: undefined,
-  error: "Error: Please input a real City and Country!!"
-
-});
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header> */}
+        <h1 className="App-intro">
+          Here is where the front-end of our app will live
+        </h1>
+      </div>
+    );
   }
 }
-
-render() {
-
-  return (
-
-<div className = "boxer"> 
-
-<div className ="wrapper">
-<div className ="main">
-<div className ="container">
-<div className ="row">
-
-<div className = ".col-xs-5 title-container">
-<Titles/>
-            </div>
-            <div className = ".col-xs-7 form-container">
-            <Form showWeather={this.showWeather}/>
-            <Weather
-            tempreture={this.state.tempreture}
-            city={this.state.city}
-            country={this.state.country}
-            humidity={this.state.humidity}
-            description={this.state.description}
-            error={this.state.error}
-            />
-
-            </div>
-          </div>
-      </div>
-      </div>
-  </div>
-</div>
-
-  );
-}
-};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css"
-import "./screens/Login.css"
-import Register from '../src/screens/Register';
-import Login from '../src/screens/Login';
-import Home from '../src/App';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-const Index = ({ pathname }) => {
-    switch(pathname) {
-        case '/':
-            return <Register />
-        case '/login':
-            return <Login />
-        case '/home':
-            return <Home />
-        default:
-            return <Register />
-    }
-}
-
-let pathname = window.location.pathname;
-
-ReactDOM.render(
-    <Index pathname={pathname} />,
-    document.getElementById('root')
-)
-
-window.addEventListener('popstate', () => {
-    pathname = window.location.pathname;
-})
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
